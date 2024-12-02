98da767d3dc4f7195fa3a6b218baa507934b8d27
Tom <info@tomhoward.codes>
2024-12-02 08:44:17 -0500

    style: format qrcodes

--- olympics/games/management/commands/qrcodes.py
+++ olympics/games/management/commands/qrcodes.py
@@ -1,14 +1,19 @@
-from django.core.management import base
-from olympics.games import models
-import qrcode
 import hashlib
-from django.conf import settings
 import pathlib
 
+import qrcode  # type: ignore[import]
+from django.conf import settings
+from django.core.management import base
+
+from olympics.games import models
+
+
 class Command(base.BaseCommand):
-    def handle(self, *args, **options):
+    def handle(self, *_, **__) -> None:
         for user in models.User.objects.all():
-            default_password = hashlib.sha256(f"{user.first_name}{user.last_name}".encode()).hexdigest()
+            default_password = hashlib.sha256(
+                f"{user.first_name}{user.last_name}".encode(),
+            ).hexdigest()
             img = qrcode.make(f"{settings.HOST}/auth/login/{user.username}/{default_password}")
             pathlib.Path(settings.BASE_DIR / "qrcodes").mkdir(exist_ok=True)
             img.save(settings.BASE_DIR / "qrcodes" / f"{user.username}.png")
